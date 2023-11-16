b7c078766fba4520af422baf104aef46c79fb93d
hojun313 <hojun313@naver.com>
2023-11-16 11:26:37 +0900

    드디어 로컬 환경 구성, 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import NoticeBoardPage from './pages/NoticeBoardPage';
